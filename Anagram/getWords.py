d303d5c846f4f422eeeff2c29c77fb625cee8c5d
rcampbell95 <robertgcampbell95@gmail.com>
2014-09-13 13:00:39 -0700

    Added closing file statement to getWords function

--- Anagram/getWords.py
+++ Anagram/getWords.py
@@ -1,13 +1,13 @@
 def getWords(filename):
-  input_file = open(filename,"r")
-  # The reason I was thinking we could have the words in a dictionary like this is so that we don't overcheck. 
-  data = {'a':[],'b':[],'c':[],'d':[],'e':[],'f':[],'g':[],'h':[],'i':[],'j':[],'k':[],'l':[],'m':[],
+	input_file = open(filename,"r")
+	# The reason I was thinking we could have the words in a dictionary like this is so that we don't overcheck. 
+	data = {'a':[],'b':[],'c':[],'d':[],'e':[],'f':[],'g':[],'h':[],'i':[],'j':[],'k':[],'l':[],'m':[],
           'n':[],'o':[],'p':[],'q':[],'r':[],'s':[],'t':[],'u':[],'v':[],'w':[],'x':[],'y':[],'z':[]}
 
-  currLetter = ord('a')
-  for line in input_file:
-    if ord(line[0]) != currLetter:
-      currLetter += 1
-    data[line[0]].append(line)
-
-  return(data)
+	currLetter = ord('a')
+	for line in input_file:
+		if ord(line[0]) != currLetter:
+			currLetter += 1
+		data[line[0]].append(line)
+	input_file.close()
+	return(data)
