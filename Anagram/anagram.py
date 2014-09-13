6fad58f7374e1a8ead8dca5a7b9efc5b9b46bd94
rcampbell95 <robertgcampbell95@gmail.com>
2014-09-12 21:41:46 -0700

    Started coding anagram.py

--- Anagram/anagram.py
+++ Anagram/anagram.py
@@ -1,3 +1,24 @@
-import getWords # importing the getWords(filename) function
+from getWords import * # importing the getWords(filename) function
 
+def isSubString(subString, wordToCheck): # This function checks to see if a word is foun
+    cpyWordToCheck = wordToCheck         # in another word
+    for char in subString:
+        if char not in cpyWordToCheck:
+            return False
+        else if char in cpyWordToCheck:
+            # I was thinking of removing characters that were already found
+            # so that errors don't happen with words with more than one
+            # of a letter.
+        return True
+        
 
+data = getWords("wordsEn.txt")
+
+word = input("> ") # Or raw_input("> "), for Python 2.7
+
+count = ord('a')
+print(count)
+for words in data[chr(count)]:
+    for word in words:
+        pass # This doesn't do anything
+    
